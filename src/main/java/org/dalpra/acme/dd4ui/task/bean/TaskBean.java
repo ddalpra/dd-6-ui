d6e9ef2d5badef5f03737e76832cff816c1b30b1
ddalpra <d.dalpra86@gmail.com>
2022-12-13 23:03:30 +0100

    SVIL-2 aggiornamento

--- src/main/java/org/dalpra/acme/dd4ui/task/bean/TaskBean.java
+++ src/main/java/org/dalpra/acme/dd4ui/task/bean/TaskBean.java
@@ -7,6 +7,7 @@ import org.dalpra.acme.dd4ui.task.entity.Task;
 import org.jboss.resteasy.client.jaxrs.ResteasyClient;
 import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
 
+import jakarta.faces.application.FacesMessage;
 import jakarta.faces.context.FacesContext;
 import jakarta.faces.view.ViewScoped;
 import jakarta.inject.Inject;
@@ -85,5 +86,21 @@ public class TaskBean implements Serializable {
 		this.selectedTasks = selectedTasks;
 	}
 	
+	public void openNew() {
+		task = new Task();
+	}
+	
+	public void saveTask() {
+		addMessage("Save", "Data saved");
+		
+		
+		
+		
+	}
+	
+	 public void addMessage(String summary, String detail) {
+	        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, summary, detail);
+	        FacesContext.getCurrentInstance().addMessage(null, message);
+	    }
 	
 }
